9241a2f1a3c9021cc426d0f734afe42d93902608
王时信 <2362452964@qq.com>
2023-10-31 17:12:16 +0800

    1031-commit Migration Log

---
+++ Services/leo.-delivery.-services/Delivery.EntityFramework.Core/Migrations/OrderDb/20231031090655_Adjuest ItemHeight Type。.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Delivery.EntityFramework.Core.Migrations.OrderDb
+{
+    /// <inheritdoc />
+    public partial class AdjuestItemHeightType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "order_ItemHight",
+                table: "sys_Order",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "order_ItemHight",
+                table: "sys_Order",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
