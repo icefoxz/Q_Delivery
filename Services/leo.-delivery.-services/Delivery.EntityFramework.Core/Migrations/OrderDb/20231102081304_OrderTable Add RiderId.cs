e555d6a5bd787c80d7c6a24943dc5440ed1acaa2
王时信 <2362452964@qq.com>
2023-11-02 16:13:49 +0800

    1102-execute Migration

---
+++ Services/leo.-delivery.-services/Delivery.EntityFramework.Core/Migrations/OrderDb/20231102081304_OrderTable Add RiderId.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Delivery.EntityFramework.Core.Migrations.OrderDb
+{
+    /// <inheritdoc />
+    public partial class OrderTableAddRiderId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "order_RiderId",
+                table: "sys_Order",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "order_RiderId",
+                table: "sys_Order");
+        }
+    }
+}
